2f570457678f8ba6c77b363c1437bbf90394d470
rohanmahdik <93382658+RohanMahadik@users.noreply.github.com>
2025-01-05 22:26:48 +0530

    initial comment

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+// js for banner slider
+
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 18,
+    nav: false,
+    dots: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 5000,
+
+})
+
+
+// for header fixed
+
